e316b3edecd063d5d08809d4b6e7b515a46ce59c
kenzochen <chenjiang@xywifi.com>
2020-01-10 14:50:13 +0800

    初始化eslint和prettier配置

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true
+  },
+  extends: ['react-app', 'airbnb', 'plugin:prettier/recommended'],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    semi: ['error', 'always'],
+    camelcase: 'off',
+    eqeqeq: 'off',
+    'no-trailing-spaces': [
+      'warn',
+      { skipBlankLines: true, ignoreComments: true }
+    ],
+    'comma-dangle': ['error', 'only-multiline'],
+    'no-unused-expressions': [
+      'error',
+      { allowShortCircuit: true, allowTernary: true }
+    ],
+    'func-names': 'off',
+    'no-unused-vars': [
+      'error',
+      { varsIgnorePattern: 'React', caughtErrors: 'none', args: 'after-used' }
+    ]
+  }
+};
